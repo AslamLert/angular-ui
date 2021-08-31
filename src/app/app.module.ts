39585689159019cee3c455802ae6bc8b7ba1a1e9
AslamLert <gloommyday@gmail.com>
2021-09-01 00:37:37 +0700

    route?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//import { RouterModule, Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './componants/employees/employees.component';
+
+//const appRouters: Router = [
+//  { path: '', component:EmployeesComponent}
+//];
 
 @NgModule({
   declarations: [
@@ -13,6 +18,7 @@ import { EmployeesComponent } from './componants/employees/employees.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    //,RouterModule.forRoot(appRouters)
   ],
   providers: [],
   bootstrap: [AppComponent]
